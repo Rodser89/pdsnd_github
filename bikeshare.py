951561e5b08e65c90c09c27576d46c464a8e5a97
Rodrigo Serrano <rodser89@gmail.com>
2021-10-10 19:42:02 -0600

    Define formats of files

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ import time
 import pandas as pd
 import numpy as np
 
-#city_data include names of cities format csv
+#city_data include name of cities format csv
 CITY_DATA = { 'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv' }
